26e3c64ab71b508b5eb524925a61fa12e2f1b358
mehdifersi <mahdi.fersi@esprit.tn>
2023-02-03 17:49:15 +0100

    second commit

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,6 +9,8 @@ import {Product} from "../core/model/product";
 export class ProductComponent implements OnInit{
   title:string ="welcome 4se "
   listProduct!:Product[]
+  test:boolean=true
+  priceMax!:number;
   constructor() {
   }
   ngOnInit():void {
@@ -16,5 +18,12 @@ export class ProductComponent implements OnInit{
       {id: 1, title: "T-shirt1", price: 18, quantity: 0, like: 0},
       {id: 2, title: "T-shirt2", price: 21, quantity: 10, like: 0},
       {id: 3, title: "T-shirt3", price: 16, quantity: 8, like: 0}, ]
+  }
+   decremente(i:number){
+
+      this.listProduct[i].quantity--;
+  }
+  incremente(i:number){
+    this.listProduct[i].like++;
   }
 }
