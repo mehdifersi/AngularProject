e1cb58032063df79ba41b4b7cb7e453a4d53ff35
mehdifersi <mahdi.fersi@esprit.tn>
2023-02-05 21:49:44 +0100

    TODO EX1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { HomeComponent } from './home/home.component';
 import {FormsModule} from "@angular/forms";
+import { OffresEmploiComponent } from './offres-emploi/offres-emploi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    HomeComponent
+    HomeComponent,
+    OffresEmploiComponent
   ],
   imports: [
     BrowserModule,
